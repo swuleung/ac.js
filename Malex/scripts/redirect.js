ec3ad14e4af585be63c54e190320457655de7255
Jeffrey Zou <jeffrey.zou@stonybrook.edu>
2018-11-23 21:00:40 -0500

    Redirect url is not being set

--- Malex/scripts/redirect.js
+++ Malex/scripts/redirect.js
@@ -1,21 +1,29 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(req) {
+        redirectURL = '';
         chrome.identity.getProfileUserInfo(function(info) {
             if (req.initiator !== 'chrome-extension://' + chrome.runtime.id) {
                 // Fetch the secure and redirect urls
                 userEmail = JSON.stringify(info.email);
                 userEmail = userEmail.substring(1, userEmail.length-1);
                 $.get(`http://localhost:5000/get_secure/${userEmail}`, function(data1) {
-                    if (info.url in data1.s) {
+                    found = false;
+                    for (sUrl of data1.s) {
+                        if (req.url.search(sUrl) > -1 || sUrl.search(req.url) > -1) {
+                            found = true;
+                        }
+                    }
+                    if (found) {
                         $.get(`http://localhost:5000/get_random/${userEmail}`, function(data2) {
                             redirectURL = data2.r[Math.floor(Math.random() * data2.r.length)];
-                            console.log("Redirect to: " + redirectURL);
-                            return { redirectUrl: redirectURL }; // Fix
                         });
                     }
                 });
             }
         });
+        return {
+            redirectUrl: redirectURL
+        }
     }, 
     {
         urls: ["<all_urls>"],
@@ -23,3 +31,65 @@ chrome.webRequest.onBeforeRequest.addListener(
     },
     ["blocking"]
 );
+
+/*
+function temp(req) {
+    redirectURL = '';
+    return new Promise(resolve => {
+        chrome.identity.getProfileUserInfo(function(info) {
+            if (req.initiator !== 'chrome-extension://' + chrome.runtime.id) {
+                // Fetch the secure and redirect urls
+                userEmail = JSON.stringify(info.email);
+                userEmail = userEmail.substring(1, userEmail.length-1);
+                $.get(`http://localhost:5000/get_secure/${userEmail}`, function(data1) {
+                    found = false;
+                    for (sUrl of data1.s) {
+                        if (req.url.search(sUrl) > -1 || sUrl.search(req.url) > -1) {
+                            found = true;
+                        }
+                    }
+                    if (found) {
+                        $.get(`http://localhost:5000/get_random/${userEmail}`, function(data2) {
+                            console.log("SECOND GET");
+                            console.log("hello");
+                            redirectURL = data2.r[Math.floor(Math.random() * data2.r.length)];
+                            console.log(redirectURL);
+                        });
+                    }
+                });
+            }
+        });
+    }).then(() => {
+        console.log(redirectURL);
+        console.log("IN THE THEN");
+        return redirectURL;
+    });
+}*/
+
+/*
+async function getRedirectURL(req) {
+    if (req.initiator === 'chrome-extension://' + chrome.runtime.id)
+        return '';
+    return await chrome.identity.getProfileUserInfo(function(info) {
+        // Fetch the secure and redirect urls
+        userEmail = JSON.stringify(info.email);
+        userEmail = userEmail.substring(1, userEmail.length-1);
+        $.get(`http://localhost:5000/get_secure/${userEmail}`, function(data1) {
+            console.log("FIRST GET");
+            found = false;
+            for (sUrl of data1.s) {
+                if (req.url.search(sUrl) > -1 || sUrl.search(req.url) > -1) {
+                    found = true;
+                }
+            }
+            if (found) {
+                console.log("INSIDE IF");
+                $.get(`http://localhost:5000/get_random/${userEmail}`, function(data2) {
+                    console.log("SECOND GET");
+                    redirectURL = data2.r[Math.floor(Math.random() * data2.r.length)];
+                    console.log("Redirect to: " + redirectURL);
+                });
+            }
+        });
+    });
+}*/
