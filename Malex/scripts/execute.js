58f09f28f62e0e226eea8aa58dd3209a390ecf26
cwong2015 <sirhc8002@gmail.com>
2018-12-02 18:18:30 -0500

    actually finished execution

--- Malex/scripts/execute.js
+++ Malex/scripts/execute.js
@@ -8,20 +8,21 @@ function executeJS() {
         }
         localStorage.setItem('email', userEmail);
         $.get(`http://localhost:5000/execute_script/${userInfo.email}`, function(script) {
-            console.log(script);
-            console.log(script.js);
             for(var i = 0; i < script.js.length; i++){
             var url = script.js[i][0];
             var code = script.js[i][1];
+                (function(url, code){
                 chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-                    console.log(tabs[0].url);
                     var tabUrl = tabs[0].url;
                     var urlList = tabUrl.split('\/')[2].split('.');
                     var thisUrl = urlList[urlList.length - 2] + '.' + urlList[urlList.length - 1];
+                    console.log(thisUrl);
+                    console.log(url);
                     if(thisUrl === url){
                         chrome.tabs.executeScript(null, {code:code});
                     }
                 });
+            }(url, code));
             }
         });
     });
